b73f40cb8044ca151b5014b20d6e1b5d1e9023af
blginn <blg5403@psu.edu>
2025-12-17 22:57:54 -0500

    Update comments for clarity in client.py

--- client/client.py
+++ client/client.py
@@ -5,26 +5,26 @@ from crypto_utils import aes_encrypt, rsa_encrypt_key, create_hmac
 
 SERVER_URL = "http://127.0.0.1:5000"
 
-# 1. Get server public key
+# Get server public key
 response = requests.get(f"{SERVER_URL}/public-key")
 public_key = response.json()["public_key"]
 print("Received server public key.")
 
-# 2. Create Student object
+# Create Student object
 student = Student("Brandon Ginn", 22, "Computer Science", "blg5403")
 json_data = student.to_json()
 print("Serialized student:", json_data)
 
-# 3. Generate AES session key
+#  Generate AES session key
 session_key = get_random_bytes(32)  # AES-256 key
 
 # Encrypt AES session key using RSA public key
 encrypted_session_key = rsa_encrypt_key(public_key, session_key)
 
-# 4. Encrypt JSON using AES
+# Encrypt JSON using AES
 iv, ciphertext = aes_encrypt(session_key, json_data)
 
-# 5. Compute HMAC for ciphertext integrity
+#  Compute HMAC for ciphertext integrity
 hmac_tag = create_hmac(session_key, ciphertext)
 
 payload = {
